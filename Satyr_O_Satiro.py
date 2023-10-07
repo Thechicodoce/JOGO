97f4c25cc22b3f992eb776dd11344a5ac9ada646
Renan <renanramalho_7@hotmail.com>
2023-10-07 17:49:11 -0300

    beta0.4

--- Satyr_O_Satiro.py
+++ Satyr_O_Satiro.py
@@ -13,16 +13,55 @@ def anima_player():
         player_index = 0
 
 def anima_bot():
-    pass
+    global player_index
 
-def rolagem_dado():
-    rolagem_satiro = (randint(0, 20))
-    rolagem_bot = (randint(0, 20))
+    tela.blit(minotauro_parado[int(player_index)], player2_rect)
+    player_index += 0.05
+    if player_index > len(minotauro_parado):
+        player_index = 0
+
+def rolagem():
+    return randint(0, 20)
+
+
+def placar_satiro():
+    #rolagem_satiro = rolagem()
+    global potion1
+    #Muda a cor da opçao se nao exister a possibilidade de agir
+    if potion1 <= 0:
+        cor_acao2 = '#1A1A1A'
+    elif potion1 > 0:
+        cor_acao2 = '#FFFFFF'
+    
+    #Carrega dados do placar Satiro
+    #txt_dado = fonte_pixel.render(f'Sua rolagem foi: {rolagem_satiro}', True, '#FFFFFF')
+    txt_life = fonte_pixel.render(f'Vida : {life_satiro}', True, '#D40000')
+    txt_potion1 = fonte_pixel.render(f'Potion: {potion1}', True, '#1AD400')
+    txt_acao1 = fonte_pixel.render(f'1 - ATACAR', True, '#FFFFFF')
+    txt_acao2 = fonte_pixel.render(f'2 - CURAR', True, f'{cor_acao2}')
+    
+    tela.blit(txt_life, (55,30))
+    tela.blit(txt_potion1, (55, 60))
+    tela.blit(txt_acao1, (55, 90))
+    tela.blit(txt_acao2, (55, 120))
+    #tela.blit(txt_dado, (55, 150))
+
+def placar_bot():
+    
+    txt_life = fonte_pixel.render(f'Vida {life_satiro}', True, '#D40000')
+    txt_potion2 = fonte_pixel.render(f'Potion: {potion2}', True, '#1AD400')
+    tela.blit(txt_life, (1115,30))
+    tela.blit(txt_potion2, (1115, 60))
+    
 
 #Inicializa o pygame
 pygame.init()
 
 #Variáveis
+life_satiro = 100
+life_bot = 100
+potion1 = 0
+potion2 = 1
 
 #Cria a tela
 tamanho = (1280, 720)
@@ -43,6 +82,7 @@ img_vela = pygame.image.load('assets/fundo/fundo2/vela.png').convert_alpha()
 img_arco = pygame.image.load('assets/fundo/fundo2/arco.png').convert_alpha()
 img_chao = pygame.image.load('assets/fundo/fundo2/chao.png').convert_alpha()
 img_estatua = pygame.image.load('assets/fundo/fundo2/estatua.png').convert_alpha()
+img_placar = pygame.image.load('assets/fundo/fundo2/placar.png').convert_alpha()
 
 #Altera a escala do plano de fundo
 img_fundo = pygame.transform.scale(img_fundo, tamanho)
@@ -52,16 +92,43 @@ img_vela = pygame.transform.scale(img_vela, tamanho)
 img_arco = pygame.transform.scale(img_arco, tamanho)
 img_chao = pygame.transform.scale(img_chao, tamanho)
 img_estatua = pygame.transform.scale(img_estatua, tamanho)
+img_placar = pygame.transform.scale(img_placar, (1240, 180))
 
 #Carrega personagens
 player_index = 0
 satiro_parado = []
-minotauro_parado = []
+satiro_ataque = []
+satiro_andar = []
+satiro_voltar = []
 
+minotauro_parado = []
+minotauro_ataque = []
+minotauro_andar = []
+
+    #Satiro parado
 for imagem in range(1, 13):
     img = pygame.image.load(f'assets/personagem/satiro1/ocio2/ocio2{imagem}.png').convert_alpha()
     img = pygame.transform.scale(img, (420, 320))
     satiro_parado.append(img)
+
+    #Satiro andando
+for imagem in range(1, 19):
+    img = pygame.image.load(f'assets/personagem/satiro1/andar/andar1{imagem}.png').convert_alpha()
+    img = pygame.transform.scale(img, (420,320))
+    satiro_andar.append(img)
+
+    #Satiro voltando
+for imagem in range(1, 19):
+    img = pygame.image.load(f'assets/personagem/satiro1/andar/andar1{imagem}.png').convert_alpha()
+    img = pygame.transform.scale(img, (420,320))
+    img = pygame.transform.flip(img, True, False)
+    satiro_voltar.append(img)
+
+    #Satiro atacando
+for imagem in range(1, 13):
+    img = pygame.load(f'assets/personagem/satiro1/ataque/ataque1{imagem}.png').convert_alpha()
+    img = pygame.transform.scale(img, (420,320))
+    satiro_ataque.append(img)
 
 for imagem in range(1, 19):
     img = pygame.image.load(f'assets/personagem/minotauro1/ocio2/ocio2{imagem}.png').convert_alpha()
@@ -109,17 +176,24 @@ while True:
     tela.blit(img_arco, (0, 0))
     tela.blit(img_chao, (0,0))
     tela.blit(img_estatua, (0,0))
+    tela.blit(img_placar, (20, 20))
 
-    #Desenha o personagem
-    tela.blit(satiro_parado[int(player_index)], player_rect)
-    player_index += 0.05
-    if player_index > len(satiro_parado):
-        player_index = 0
+    #Chama animação do personagens
+    anima_player()
+    anima_bot()
 
-    tela.blit(minotauro_parado[int(player_index)], player2_rect)
-    player_index += 0.05
-    if player_index > len(minotauro_parado):
-        player_index = 0
+    #Chama Rolagem de dados
+    rolagem()
+
+    #Chama Placares
+    placar_satiro()
+    placar_bot()
+
+    #Encontro
+    tela.blit(fonte_pixel.render('UM MINOTAURO APARECEU!', True, '#FFFFFF'), (450, 30))
+    tela.blit(fonte_pixel.render('(GRRRRRRRRR!!!)', True, '#FFFFFF'), (500, 60))
+    tela.blit(fonte_pixel.render('Escolha o que vai fazer', True, '#FFFFFF'), (460, 90))
+
 
     #Atualiza a tela com o conteudo
     pygame.display.update()
@@ -127,10 +201,4 @@ while True:
     #Define a quantidade de frames por segundo
     relogio.tick(60)
 
-    #Chama animação do personagens
-    anima_player()
-    anima_bot()
-
-    #Chama Rolagem de dados
-    rolagem_dado
-
+    
